bbe71ef0dbb6233d444fa911186c3a1e37948fed
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-06-23 23:02:50 -0400

    Create initial UI
    Very basic UI with sidebar of scrolling icons. Selecting an icon will make it appear in the 256px preview in the center. Labels are implemented but not enabled due to a bug. Light & dark modes are supported in beta. Not all final icons are available yet.

---
+++ js.js
@@ -0,0 +1,23 @@
+var icons = ["Calculator", "Calendar", "Camera", "ClipChamp", "Clock" , "Family Safety Monitor" , "Feedback Hub" , "File Explorer (Windows 11)" , "Get Help" , "Get Started" , "Journal" , "Live Captions" , "Magnify" , "Mail" , "Maps" , "Media Player" , "Messaging" , "Microsoft News" , "Microsoft Store (Light)" , "Microsoft Store (Dark)" , "Microsoft To Do"];
+var indxAmt = icons.length;
+var temp;
+var previewBox = document.getElementById('lrgPrev');
+
+typeof globalThis;
+
+var gridBox = document.getElementById('grid'),
+    iconBox = document.getElementById('icon'),
+    clone;
+
+    icons.forEach(function (iconInd, index) {
+    clone = iconBox.cloneNode();
+    clone.setAttribute('style', 'background-image: url("/Icons/' + iconInd + '.png")');
+    clone.setAttribute('data-iconName', iconInd);
+    gridBox.appendChild(clone);
+
+    clone.onclick = function () {
+        previewBox.setAttribute('style', 'background-image: url("/Icons/' + iconInd + '.png")');
+        previewBox.setAttribute('data-iconName', clone.getAttribute('data-iconName'));
+    };
+
+});
