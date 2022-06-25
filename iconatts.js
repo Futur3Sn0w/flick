5919ad15a19667376a433d896b2ae5b4ba2f6341
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-06-25 15:04:22 -0400

    (0.3.0)
    Icons with a dark mode will now show a toggle to switch to it.

--- iconatts.js
+++ iconatts.js
@@ -1,91 +1,109 @@
 const icons = [
       {
         "path": "calculator.png",
+        "darkMode": "no",
         "friendlyName": "Calculator",
         "firstSeen": "10X"
       },
       {
         "path": "calendar.png",
+        "darkMode": "no",
         "friendlyName": "Calendar",
         "firstSeen": "10X"
       }, 
       {
         "path": "camera.png",
+        "darkMode": "no",
         "friendlyName": "Camera",
         "firstSeen": "10X"
       }, 
       {
         "path": "clipchamp.png",
+        "darkMode": "no",
         "friendlyName": "ClipChamp",
         "firstSeen": "10X"
       }, 
       {
         "path": "clock.png",
+        "darkMode": "no",
         "friendlyName": "Clock",
         "firstSeen": "10X"
       }, 
       {
         "path": "familysafetymonitor.png",
+        "darkMode": "no",
         "friendlyName": "Family Safety Monitor",
         "firstSeen": "10X"
       }, 
       {
         "path": "feedbackhub.png",
+        "darkMode": "no",
         "friendlyName": "Feedback Hub",
         "firstSeen": "10X"
       }, 
       {
         "path": "fileexplorer.png",
+        "darkMode": "no",
         "friendlyName": "File Explorer",
         "firstSeen": "10X"
       }, 
       {
         "path": "gethelp.png",
+        "darkMode": "no",
         "friendlyName": "Get Help",
         "firstSeen": "10X"
       }, 
       {
         "path": "getstarted.png",
+        "darkMode": "no",
         "friendlyName": "Get Started",
         "firstSeen": "10X"
       }, 
       {
         "path": "journal.png",
+        "darkMode": "no",
         "friendlyName": "Journal",
         "firstSeen": "10X"
       }, 
       {
         "path": "livecaptions.png",
+        "darkMode": "no",
         "friendlyName": "Live Captions",
         "firstSeen": "10X"
       }, 
       {
         "path": "magnify.png",
+        "darkMode": "no",
         "friendlyName": "Magnifier",
         "firstSeen": "10X"
       }, 
       {
         "path": "mail.png",
+        "darkMode": "no",
         "friendlyName": "Mail",
         "firstSeen": "10X"
       }, 
       {
         "path": "maps.png",
+        "darkMode": "no",
         "friendlyName": "Maps",
         "firstSeen": "10X"
       }, 
       {
         "path": "mediaplayer.png",
+        "darkMode": "no",
         "friendlyName": "Media Player",
         "firstSeen": "10X"
       }, 
       {
         "path": "messaging.png",
+        "darkMode": "no",
         "friendlyName": "Messaging",
         "firstSeen": "10X"
       }, 
       {
         "path": "microsoftnews.png",
+        "darkMode": "no",
         "friendlyName": "Microsoft News",
         "firstSeen": "10X"
       }, 
@@ -97,116 +115,139 @@ const icons = [
       }, 
       {
         "path": "microsofttodo.png",
+        "darkMode": "no",
         "friendlyName": "Microsoft To Do",
         "firstSeen": "10X"
       }, 
       {
         "path": "microsoftedge.png",
+        "darkMode": "no",
         "friendlyName": "Microsoft Edge",
         "firstSeen": "10X"
       }, 
       {
         "path": "mixedrealityportal.png",
+        "darkMode": "no",
         "friendlyName": "Mixed Reality Portal",
         "firstSeen": "10X"
       }, 
       {
         "path": "moviesandtv.png",
+        "darkMode": "no",
         "friendlyName": "Movies & TV",
         "firstSeen": "10X"
       }, 
       {
         "path": "narrator.png",
+        "darkMode": "no",
         "friendlyName": "Narrator",
         "firstSeen": "10X"
       }, 
       {
         "path": "notepad.png",
+        "darkMode": "no",
         "friendlyName": "Notepad",
         "firstSeen": "10X"
       }, 
       {
         "path": "office.png",
+        "darkMode": "no",
         "friendlyName": "Office",
         "firstSeen": "10X"
       }, 
       {
         "path": "osk.png",
+        "darkMode": "no",
         "friendlyName": "On-Screen Keyboard",
         "firstSeen": "10X"
       }, 
       {
         "path": "d3dmapping.png",
+        "darkMode": "no",
         "friendlyName": "OpenCL™ and OpenGL® Compatibility Pack",
         "firstSeen": "10X"
       }, 
       {
         "path": "paint.png",
+        "darkMode": "no",
         "friendlyName": "Paint",
         "firstSeen": "10X"
       }, 
       {
         "path": "paint3d.png",
+        "darkMode": "no",
         "friendlyName": "Paint 3D",
         "firstSeen": "10X"
       }, 
       {
         "path": "people.png",
+        "darkMode": "no",
         "friendlyName": "People",
         "firstSeen": "10X"
       }, 
       {
         "path": "phonelink.png",
+        "darkMode": "no",
         "friendlyName": "Phone Link",
         "firstSeen": "10X"
       }, 
       {
         "path": "photos.png",
+        "darkMode": "no",
         "friendlyName": "Photos",
         "firstSeen": "10X"
       }, 
       {
         "path": "powerautomate.png",
+        "darkMode": "no",
         "friendlyName": "Power Automate",
         "firstSeen": "10X"
       }, 
       {
         "path": "powerbidesktop.png",
+        "darkMode": "no",
         "friendlyName": "Power BI Desktop",
         "firstSeen": "10X"
       }, 
       {
         "path": "powertoys.png",
+        "darkMode": "no",
         "friendlyName": "PowerToys",
         "firstSeen": "10X"
       }, 
       {
         "path": "printqueueactioncenter.png",
+        "darkMode": "no",
         "friendlyName": "Print Management",
         "firstSeen": "10X"
       }, 
       {
         "path": "quickassist.png",
+        "darkMode": "no",
         "friendlyName": "Quick Assist",
         "firstSeen": "10X"
       }, 
       {
         "path": "remotedesktop.png",
+        "darkMode": "no",
         "friendlyName": "Remote Desktop Connection",
         "firstSeen": "10X"
       }, 
       {
         "path": "settings.png",
+        "darkMode": "no",
         "friendlyName": "Settings",
         "firstSeen": "10X"
       }, 
       {
         "path": "snippingtool.png",
+        "darkMode": "no",
         "friendlyName": "Snipping Tool",
         "firstSeen": "10X"
       }, 
       {
         "path": "solitarecollection.png",
+        "darkMode": "no",
         "friendlyName": "Solitare Collection",
         "firstSeen": "10X"
       }, 
@@ -218,61 +259,73 @@ const icons = [
       }, 
       {
         "path": "surface.png",
+        "darkMode": "no",
         "friendlyName": "Surface",
         "firstSeen": "10X"
       }, 
       {
         "path": "teams.png",
+        "darkMode": "no",
         "friendlyName": "Teams",
         "firstSeen": "10X"
       }, 
       {
         "path": "terminal.png",
+        "darkMode": "no",
         "friendlyName": "Terminal",
         "firstSeen": "10X"
       }, 
       {
         "path": "tips.png",
+        "darkMode": "no",
         "friendlyName": "Tips",
         "firstSeen": "10X"
       }, 
       {
         "path": "videoeditor.png",
+        "darkMode": "no",
         "friendlyName": "Video Editor",
         "firstSeen": "10X"
       }, 
       {
         "path": "voiceaccess.png",
+        "darkMode": "no",
         "friendlyName": "Voice Access",
         "firstSeen": "10X"
       }, 
       {
         "path": "voicerecorder.png",
+        "darkMode": "no",
         "friendlyName": "Voice Recorder",
         "firstSeen": "10X"
       }, 
       {
         "path": "weather.png",
+        "darkMode": "no",
         "friendlyName": "Weather",
         "firstSeen": "10X"
       }, 
       {
         "path": "windowssecurity.png",
+        "darkMode": "no",
         "friendlyName": "Windows Security",
         "firstSeen": "10X"
       }, 
       {
         "path": "linuxsubsystem.png",
+        "darkMode": "no",
         "friendlyName": "Windows Subsystem for Linux Preview",
         "firstSeen": "10X"
       }, 
       {
         "path": "winfr.png",
+        "darkMode": "no",
         "friendlyName": "Windows File Recovery",
         "firstSeen": "10"
       }, 
       {
         "path": "xboxgamebar.png",
+        "darkMode": "no",
         "friendlyName": "Xbox Game Bar",
         "firstSeen": "11"
       }
@@ -280,6 +333,7 @@ const icons = [
 
 var indxAmt = icons.length;
 var previewBox = document.getElementById('lrgPrev');
+var darkToggle = document.getElementById('darkToggle');
 
 var gridBox = document.getElementById('grid'),
     iconBox = document.getElementById('icon'),
@@ -289,13 +343,37 @@ var gridBox = document.getElementById('grid'),
     clone = iconBox.cloneNode();
     clone.setAttribute('style', 'background-image: url("iconimgs/' + iconInd["path"] + '")');
     clone.setAttribute('data-iconName', iconInd["friendlyName"]);
+    clone.setAttribute('data-darkMode', iconInd["darkMode"]);
     gridBox.appendChild(clone);
 
     clone.onclick = function () {
         // alert('background-image: url("iconimgs/' + iconInd["path"] + '")');
         previewBox.setAttribute('style', 'background-image: url("iconimgs/' + iconInd["path"] + '")');
+        localStorage.setItem('lightImg', 'background-image: url("iconimgs/' + iconInd["path"] + '")');
+        localStorage.setItem('darkImg', 'background-image: url("iconimgs/' + iconInd["darkMode"] + '")');
         previewBox.setAttribute('data-iconName', iconInd["friendlyName"]);
+        previewBox.setAttribute('data-darkMode', clone.getAttribute('data-darkMode'));
         // iconBox.setAttribute("class", "icon selected");
+
+        if (iconInd['darkMode'] === "no") {
+          darkToggle.style.display = "none";
+        } else {
+          darkToggle.style.display = "flex";
+        }
     };
 
+    var showDarkIcon = false;
+
+    darkToggle.onclick = function () {
+    previewBox.setAttribute('data-showDarkIcon', showDarkIcon);
+    showDarkIcon = !showDarkIcon;
+
+    if (showDarkIcon == true) {
+      previewBox.setAttribute('style', localStorage.getItem('darkImg'));
+    } else {
+      previewBox.setAttribute('style', localStorage.getItem('lightImg'));
+    }
+  }
+
 });
+
