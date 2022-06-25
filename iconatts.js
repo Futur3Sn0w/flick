b2f3a770f69e76ce42d562fd4bbeff20cd65b43d
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-06-25 13:16:23 -0400

    (0.2.0)
    Added more icons and moved to a JSON-based collection for easier metadata editing.
    New collapsing sidebar with labels.

---
+++ iconatts.js
@@ -0,0 +1,300 @@
+const icons = [
+      {
+        "path": "calculator.png",
+        "friendlyName": "Calculator",
+        "firstSeen": "10X"
+      },
+      {
+        "path": "calendar.png",
+        "friendlyName": "Calendar",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "camera.png",
+        "friendlyName": "Camera",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "clipchamp.png",
+        "friendlyName": "ClipChamp",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "clock.png",
+        "friendlyName": "Clock",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "familysafetymonitor.png",
+        "friendlyName": "Family Safety Monitor",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "feedbackhub.png",
+        "friendlyName": "Feedback Hub",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "fileexplorer.png",
+        "friendlyName": "File Explorer",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "gethelp.png",
+        "friendlyName": "Get Help",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "getstarted.png",
+        "friendlyName": "Get Started",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "journal.png",
+        "friendlyName": "Journal",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "livecaptions.png",
+        "friendlyName": "Live Captions",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "magnify.png",
+        "friendlyName": "Magnifier",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "mail.png",
+        "friendlyName": "Mail",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "maps.png",
+        "friendlyName": "Maps",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "mediaplayer.png",
+        "friendlyName": "Media Player",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "messaging.png",
+        "friendlyName": "Messaging",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "microsoftnews.png",
+        "friendlyName": "Microsoft News",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "microsoftstore_light.png",
+        "darkMode": "microsoftstore_dark.png",
+        "friendlyName": "Microsoft Store",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "microsofttodo.png",
+        "friendlyName": "Microsoft To Do",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "microsoftedge.png",
+        "friendlyName": "Microsoft Edge",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "mixedrealityportal.png",
+        "friendlyName": "Mixed Reality Portal",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "moviesandtv.png",
+        "friendlyName": "Movies & TV",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "narrator.png",
+        "friendlyName": "Narrator",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "notepad.png",
+        "friendlyName": "Notepad",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "office.png",
+        "friendlyName": "Office",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "osk.png",
+        "friendlyName": "On-Screen Keyboard",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "d3dmapping.png",
+        "friendlyName": "OpenCL™ and OpenGL® Compatibility Pack",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "paint.png",
+        "friendlyName": "Paint",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "paint3d.png",
+        "friendlyName": "Paint 3D",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "people.png",
+        "friendlyName": "People",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "phonelink.png",
+        "friendlyName": "Phone Link",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "photos.png",
+        "friendlyName": "Photos",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "powerautomate.png",
+        "friendlyName": "Power Automate",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "powerbidesktop.png",
+        "friendlyName": "Power BI Desktop",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "powertoys.png",
+        "friendlyName": "PowerToys",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "printqueueactioncenter.png",
+        "friendlyName": "Print Management",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "quickassist.png",
+        "friendlyName": "Quick Assist",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "remotedesktop.png",
+        "friendlyName": "Remote Desktop Connection",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "settings.png",
+        "friendlyName": "Settings",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "snippingtool.png",
+        "friendlyName": "Snipping Tool",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "solitarecollection.png",
+        "friendlyName": "Solitare Collection",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "stickynotes_light.png",
+        "darkMode": "stickynotes_dark.png",
+        "friendlyName": "Sticky Notes",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "surface.png",
+        "friendlyName": "Surface",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "teams.png",
+        "friendlyName": "Teams",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "terminal.png",
+        "friendlyName": "Terminal",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "tips.png",
+        "friendlyName": "Tips",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "videoeditor.png",
+        "friendlyName": "Video Editor",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "voiceaccess.png",
+        "friendlyName": "Voice Access",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "voicerecorder.png",
+        "friendlyName": "Voice Recorder",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "weather.png",
+        "friendlyName": "Weather",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "windowssecurity.png",
+        "friendlyName": "Windows Security",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "linuxsubsystem.png",
+        "friendlyName": "Windows Subsystem for Linux Preview",
+        "firstSeen": "10X"
+      }, 
+      {
+        "path": "winfr.png",
+        "friendlyName": "Windows File Recovery",
+        "firstSeen": "10"
+      }, 
+      {
+        "path": "xboxgamebar.png",
+        "friendlyName": "Xbox Game Bar",
+        "firstSeen": "11"
+      }
+]
+
+var indxAmt = icons.length;
+var previewBox = document.getElementById('lrgPrev');
+
+var gridBox = document.getElementById('grid'),
+    iconBox = document.getElementById('icon'),
+    clone;
+
+    icons.forEach(function (iconInd, index) {
+    clone = iconBox.cloneNode();
+    clone.setAttribute('style', 'background-image: url("Icons/' + iconInd["path"] + '")');
+    clone.setAttribute('data-iconName', iconInd["friendlyName"]);
+    gridBox.appendChild(clone);
+
+    clone.onclick = function () {
+        previewBox.setAttribute('style', 'background-image: url("Icons/' + iconInd["path"] + '")');
+        previewBox.setAttribute('data-iconName', iconInd["friendlyName"]);
+        iconBox.setAttribute("class", "icon selected");
+    };
+
+});
