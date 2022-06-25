38d4293864b453c5ffcb1f11ce827d5314b3d0e7
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-06-25 13:25:01 -0400

    (0.2.1)
    Fixed icon path bug

--- iconatts.js
+++ iconatts.js
@@ -287,14 +287,14 @@ var gridBox = document.getElementById('grid'),
 
     icons.forEach(function (iconInd, index) {
     clone = iconBox.cloneNode();
-    clone.setAttribute('style', 'background-image: url("Icons/' + iconInd["path"] + '")');
+    clone.setAttribute('style', 'background-image: url("icons/' + iconInd["path"] + '")');
     clone.setAttribute('data-iconName', iconInd["friendlyName"]);
     gridBox.appendChild(clone);
 
     clone.onclick = function () {
-        previewBox.setAttribute('style', 'background-image: url("Icons/' + iconInd["path"] + '")');
+        previewBox.setAttribute('style', 'background-image: url("icons/' + iconInd["path"] + '")');
         previewBox.setAttribute('data-iconName', iconInd["friendlyName"]);
-        iconBox.setAttribute("class", "icon selected");
+        // iconBox.setAttribute("class", "icon selected");
     };
 
 });
